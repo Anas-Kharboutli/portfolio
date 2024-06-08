e90029e8fec9a98ba44a0d4a92baef0087f599ba
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-06-08 17:30:56 +0200

    fixing useEffect error

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import { NavLink, useNavigate } from 'react-router-dom';
