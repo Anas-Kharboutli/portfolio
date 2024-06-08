3e22c96f39bf49419987ec1f5d5a3f146178206a
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-06-08 17:34:34 +0200

    testing contineous deployment

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = () => {
   return (
     <section className='blog-container section__padding'>
       <h1>My technical blog is in preparation..... </h1>
-      <h2>Coming Soon</h2>
+      <h2>Coming Soon..</h2>
     </section>
   )
 }
